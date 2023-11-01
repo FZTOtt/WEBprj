85562087ec69dba3516db967def921064b14f041
fzto <fzto@Ubuntu.myguest.virtualbox.org>
2023-11-01 21:42:48 +0300

    fix

--- app/models.py
+++ app/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    author = models.ForeignKey('Author', max_length=256, on_delete=models.PROTECT)
+    date_written = models.DateField(null=True, blank=True)
+    genre = models.ForeignKey('Genre', related_name='books')
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=256)
+    surname = models.CharField(max_length=256)
+    birth_date = models.DateField()
+    death_date = models.DateField(null=True, blank=True)
+    is_deleted = models.BooleanField(default=False)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class BookInstance(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.PROTECT)
+    user = models.ForeignKey(User,on_delete=models.PROTECT)
+
+    STATUS_CHOICES = (
+        ('m', 'Maintenance'),
+        ('a', 'Avaible'),
+        ('t', 'Taken'),
+    )
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='m')
+
+    due_date = models.DateField()
--- app/views.py
+++ app/views.py
@@ -1,29 +1,49 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.http import HttpResponseBadRequest
 
 # Create your views here.
 QUESTIONS = [
         {
         'id': i,
         'title': f'Question {i}',
-        'content': f'dgjsudfg {i}'
+        'content': f'Please, help me {i}'
     } for i in range (20)
     ]
 
-def paginate(objects, page, per_page=15):
+
+ANSWERS = [
+        {
+        'id': i,
+        'title': f'Answer {i}',
+        'content': f'Let me help you {i}'
+    } for i in range (20)
+    ]
+
+
+def paginate(objects, page, per_page=5):
     paginator = Paginator(objects, per_page)
     #page_items = paginator.page(1).object_list
     return paginator.page(page)
 def index(request):
 
     page = request.GET.get('page', 1)
-
+    try:
+        page = int(page)
+    except ValueError:
+        return HttpResponseBadRequest()
     return render(request, "index.html", {'questions': paginate(QUESTIONS, page)})
 
 
 def question(request, question_id):
     item = QUESTIONS[question_id]
-    return render(request, "question.html", {'question': item})
+    page = request.GET.get('page', 1)
+    try:
+        page = int(page)
+    except ValueError:
+        return HttpResponseBadRequest()
+    return render(request, "question.html", {'question': item, 'answers' : paginate(ANSWERS, page)})
+    #return render(request, "question.html", {'question': item})
 
 
 def login(request):
